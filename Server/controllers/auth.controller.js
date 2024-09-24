c9c502905f3abed4311949a96e279e30f34ece69
Irvin <contacoirvinsamboy@gmail.com>
2024-09-23 23:05:39 -0400

    Creando el controllador de usuarios

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -2,35 +2,6 @@ import {db} from '../models/db.js'
 import bcrypt from 'bcrypt'
 import { createToken } from '../utils/token.js'
 
-export const singup = async (req, res) => {
-    const {nombre, contraseña, rol} = req.body
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassWord = await bcrypt.hash(contraseña, salt)
-    try{
-        if(!nombre || !contraseña) return res.status(400).json({message: "Uno o más campos vacios"})
-        let rolId;
-        if(rol) {
-            if(isNaN(rol)) return res.status(500).json({message: "Debes introducir el id del rol como número"})
-            const {idRol} = await db.select('idRol').where('idRol', rol).from('roles').first()
-            if(!idRol) return res.status(404).json({message: "Rol no encontrado"})
-            rolId = idRol;
-        }
-        else{
-            const {idRol} = await db.select('idRol').where('nombre', 'usuario').from('roles').first()
-            if(!idRol) return res.status(404).json({message: "Rol no encontrado"})
-            rolId = idRol;
-        }
-
-        const usuario = await db('users').insert({nombre, contraseña: hashedPassWord, idRol: rolId})
-        if(!usuario) return res.status(500).json({message: "Error al crear el usuario"})
-        return res.status(200).json({message: "Usuario creado correctamente"})
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-        console.log(error)
-    }
-
-}
 
 export const signin =  async (req, res) => {
     const {nombre, contraseña} = req.body
@@ -68,3 +39,4 @@ export const logOut = async (req, res) => {
         console.log(error)
     }
 }
+
