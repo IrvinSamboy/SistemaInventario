d9b80e449b9b26061c7fd9c5eae12020b37ce5ff
Irvin <contacoirvinsamboy@gmail.com>
2024-09-23 21:29:29 -0400

    Arreglando el controlador de empleados, errores minimos de sintaxis y haciendo pequeñas modificaciones a la base de datos. También se añadio la clausula para confirmar si el rol que se le intenta asignar al usuario viene en formato númerico

--- Server/routes/empleados.routes.js
+++ Server/routes/empleados.routes.js
@@ -5,10 +5,10 @@ import { checkAuth } from '../middlewares/checkAuth.js';
 const router = Router();
 
 // Rutas CRUD de empleados
-router.get('/empleados', checkAuth(["admin", "user"]), empleadosController.getEmpleados);
-router.get('/empleados/:id', checkAuth(["admin", "user"]), empleadosController.getEmpleadoById);
-router.post('/empleados', checkAuth(["admin"]), empleadosController.createEmpleado);
-router.put('/empleados/:id', checkAuth(["admin"]), empleadosController.updateEmpleado);
-router.delete('/empleados/:id', checkAuth(["admin"]), empleadosController.deleteEmpleado);
+router.get('/', checkAuth(["admin", "usuario"]), empleadosController.getEmpleados);
+router.get('/:id', checkAuth(["admin", "usuario"]), empleadosController.getEmpleadoById);
+router.post('/', checkAuth(["admin"]), empleadosController.createEmpleado);
+router.put('/:id', checkAuth(["admin"]), empleadosController.updateEmpleado);
+router.delete('/:id', checkAuth(["admin"]), empleadosController.deleteEmpleado);
 
 export default router;
