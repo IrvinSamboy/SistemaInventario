6b9e3cb182fba0f5b8d429fe558893c539e4d00c
Irvin <contacoirvinsamboy@gmail.com>
2024-09-29 13:07:32 -0400

    Arreglando errores del controlador de compras

--- Server/controllers/compras.controller.js
+++ Server/controllers/compras.controller.js
@@ -8,8 +8,8 @@ export const createCompra = async (req, res) => {
         const proveedor = await db.select('*').where('idProveedor', idProveedor).from('proveedores').first()
         if(!proveedor) return res.status(500).json({message: "Proveedor no encontrado"})
         const fecha = new Date()
-        const {idUser} = getToken(req.cookies.token)
-        const newCompra = db('compras').insert({idProveedor, fecha, idUser})
+        const {idUser} = await getToken(req.cookies.token)
+        const newCompra = await db('compras').insert({idProveedor, fecha, idUser})
         if(!newCompra) return res.status(500).json({message: "Ha ocurrido un error al crearla compra"})
         return res.status(200).json({message: "Compra creada correctamente"})
     }
@@ -29,7 +29,7 @@ export const updateCompra = async (req, res) => {
             } = req.body
         const proveedor = await db.select('*').where('idProveedor', idProveedor).from('proveedores').first()
         if(!proveedor) return res.status(500).json({message: "Proveedor no encontrado"})
-        const proveedorUpdated = db('compras').update({idProveedor}).where('idCompra', id)
+        const proveedorUpdated = await db('compras').update({idProveedor}).where('idCompra', id)
         if(!proveedorUpdated) return res.status(500).json({message: "Ha ocurrido un error al crearla compra"})
         return res.status(200).json({message: "Compra editada correctamente"})
     }
@@ -61,12 +61,12 @@ export const generarFactura = async (req, res) => {
         if(reporte) {
             await trx('reportes').where('idCompra', idCompra).del()
         }
-        const compra = await trx.select('compras.*', 'proveedores.nombre as nombreProveedor', 'usuarios.nombre as nombreUsuario')
+        const compra = await trx.select('compras.*', 'proveedores.nombre as nombreProveedor', 'users.nombre as nombreUsuario')
         .where('idCompra', idCompra).from('compras')
-        .join('users', 'user.IdUser', 'compra.IdUser')
-        .join('proveedores', 'proveedores.idProveedor', 'compra.idProveedor').first()
+        .join('users', 'users.IdUser', 'compras.IdUser')
+        .join('proveedores', 'proveedores.idProveedor', 'compras.idProveedor').first()
         if(!compra) return res.status(404).json({message: "Error al encontrar la compra asociada a esta factura"})
-        const detallesCompra = await trx.select('detallesCompra.*', 'productos.nombre as nombreProducto').where('idCompra', idCompra)
+        const detallesCompra = await trx.select('detallesCompra.*', 'productos.nombre as nombreProducto').where('idCompra', idCompra).from('detallesCompra')
         .join('productos', 'productos.idProducto', 'detallesCompra.idProducto')
         if(detallesCompra.length === 0)return res.status(404).json({message: "Esta compra no tiene productos"})
         await trx('reportes')
