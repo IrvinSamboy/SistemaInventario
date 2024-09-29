e7f86e51153aa24b4363cabe421e7a46c43ab06c
Irvin <contacoirvinsamboy@gmail.com>
2024-09-29 12:36:39 -0400

    Añadiendo "gerente" al manejo de empleados

--- Server/routes/empleados.routes.js
+++ Server/routes/empleados.routes.js
@@ -5,8 +5,8 @@ import { checkAuth } from '../middlewares/checkAuth.js';
 const router = Router();
 
 // Rutas CRUD de empleados
-router.get('/', checkAuth(["admin", "usuario"]), empleadosController.getEmpleados);
-router.get('/:id', checkAuth(["admin", "usuario"]), empleadosController.getEmpleadoById);
+router.get('/', checkAuth(["admin", "usuario", "gerente"]), empleadosController.getEmpleados);
+router.get('/:id', checkAuth(["admin", "usuario", "gerente"]), empleadosController.getEmpleadoById);
 router.post('/', checkAuth(["admin"]), empleadosController.createEmpleado);
 router.put('/:id', checkAuth(["admin"]), empleadosController.updateEmpleado);
 router.delete('/:id', checkAuth(["admin"]), empleadosController.deleteEmpleado);
